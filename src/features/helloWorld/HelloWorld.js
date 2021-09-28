5118bb691e21b07e72f87c82c3621c7983075421
iuliasirbu <iuliasirbu47@gmail.com>
2021-09-28 16:02:22 +0300

    commit hello world

--- src/features/helloWorld/HelloWorld.js
+++ src/features/helloWorld/HelloWorld.js
@@ -1,6 +1,16 @@
-function HelloWorld(){
-    
-    return "Hello, World! 🙌"
-} 
+import React from 'react';
+import { LoadingFakeText } from '@bit/totalsoft_oss.react-mui.kit.core';
+import { useQuery } from '@apollo/client';
+import { MY_FIRST_QUERY } from './queries/MyFirstQuery';
+
+function HelloWorld() {
+    const { loading, data } = useQuery(MY_FIRST_QUERY);
+
+    if (loading) {
+        return <LoadingFakeText lines={10} />
+    }
+
+    return data?.myFirstEndpoint
+}
 
 export default HelloWorld;
